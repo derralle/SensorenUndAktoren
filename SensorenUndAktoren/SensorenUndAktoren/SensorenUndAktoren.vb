ba5de8e16347c73506506d3eab7f57aea7b2e162
derralle <ralph@kekshirn.de>
2014-08-04 19:19:18 +0200

    sync

--- SensorenUndAktoren/SensorenUndAktoren/SensorenUndAktoren.vb
+++ SensorenUndAktoren/SensorenUndAktoren/SensorenUndAktoren.vb
@@ -33,13 +33,13 @@ Public Class SensorenUndAktoren
     Public Sub initalisierung(ByRef iswapp As SldWorks)
         _SwApp = iswapp
 
-        'Attribut definieren (Nur 1x pro Laufzeit!)
+        'Attribut definieren (Nur 1x pro Laufzeit SWX!)
         AttributDefinition = Me.SwApp.IDefineAttribute("Elektropneumatik")
         AttributDefinition.AddParameter("Hash", swParamType_e.swParamTypeString, 0.0#, 0)
         AttributDefinition.AddParameter("BMK", swParamType_e.swParamTypeString, 0.0#, 0)
         AttributDefinition.AddParameter("Bezeichnung", swParamType_e.swParamTypeString, 0.0#, 0)
 
-        'Attribut registrieren (Nur 1x pro Laufzeit!)
+        'Attribut registrieren (Nur 1x pro Laufzeit SWX!)
         AttributDefinition.Register()
     End Sub
 
@@ -68,13 +68,40 @@ Public Class SensorenUndAktoren
 
 
 
-        Att = Me.AttributDefinition.CreateInstance5(modeldoc, icomp, "Elektropneumatik", 0, swInConfigurationOpts_e.swAllConfiguration)
+        Att = Me.AttributDefinition.CreateInstance5(modeldoc, icomp, CreateUniqueFeatureName("Elektropneumatik", modeldoc), 0, swInConfigurationOpts_e.swAllConfiguration)
 
         MsgBox(Att.IGetComponent2.GetPathName())
         modeldoc.EditRebuild3()
 
-
-
-
     End Sub
+
+    Private Function CreateUniqueFeatureName(ByVal Name As String, ByRef Modeldoc As ModelDoc2) As String
+
+
+
+        Dim FeatureMgr As FeatureManager = ModelDoc.FeatureManager
+
+        Dim RetName As String = Name
+        Dim index As Long = 1
+
+
+        If FeatureMgr.IsNameUsed(swNameType_e.swFeatureName, Name) Then
+
+            While FeatureMgr.IsNameUsed(swNameType_e.swFeatureName, RetName)
+
+                RetName = Name & index.ToString
+                index = index + 1
+
+            End While
+        Else
+
+            RetName = Name
+        End If
+
+        CreateUniqueFeatureName = RetName
+
+
+
+    End Function
+
 End Class
